f738b57a070a5f79547d24e3c2d1f7c32d05aea7
Olawale <olawalesodiq988@gmail.com>
2025-08-22 03:27:53 +0100

    Worked on the toggle menu

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Mobile menu toggle for mobile screen sizes
-document.getElementById('menu-toggle').addEventListener('click', function() {
+document.querySelector(".fa-bars").addEventListener("click", function () {
     const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.toggle('active');
-});
+    navMenu.classList.add('active');
+    document.querySelector(".fa-bars").style.display = 'none';
+    document.querySelector(".fa-x").style.display = 'block';
+})
+document.querySelector(".fa-x").addEventListener("click", function () {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('active');
+    document.querySelector(".fa-x").style.display = 'none';
+    document.querySelector(".fa-bars").style.display = 'block';
+})
+
 
 // Close mobile menu when clicking on a link
 document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', function() {
         const navMenu = document.getElementById('nav-menu');
         navMenu.classList.remove('active');
+    document.querySelector(".fa-x").style.display = 'none';
+    document.querySelector(".fa-bars").style.display = 'block';
+
     });
 });
 
