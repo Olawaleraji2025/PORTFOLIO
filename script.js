576f0b9add4bd269c3a4bc64162a65b73761173a
olawale <olawalesodiq988@gmail.com>
2026-01-17 15:28:28 +0100

    `Added HTML5 and CSS3 tags to project card, updated JavaScript and CSS files for mobile responsiveness and form validation.`

--- script.js
+++ script.js
@@ -13,69 +13,11 @@ document.querySelector(".fa-x").addEventListener("click", function () {
 })
 
 
-// Close mobile menu when clicking on a link
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function() {
-        const navMenu = document.getElementById('nav-menu');
-        navMenu.classList.remove('active');
-    document.querySelector(".fa-x").style.display = 'none';
-    document.querySelector(".fa-bars").style.display = 'block';
+    
 
-    });
-});
+    
 
-// This is for the form validation
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    let isValid = true;
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    const message = document.getElementById('message');
-    
-    // Reset errors messages to none
-    document.getElementById('name-error').style.display = 'none';
-    document.getElementById('email-error').style.display = 'none';
-    document.getElementById('message-error').style.display = 'none';
-    
-    // Validate name
-    if (name.value.trim() === '') {
-        document.getElementById('name-error').style.display = 'block';
-        isValid = false;
-    }
-    
-    // Validate email
-    if (email.value.trim() === "") {
-        document.getElementById('email-error').style.display = 'block';
-        isValid = false;
-    }
-    
-    // Validate message
-    if (message.value.trim() === '') {
-        document.getElementById('message-error').style.display = 'block';
-        isValid = false;
-    }
-    
-    if (isValid) {
-        name.value = '';
-        email.value = '';
-        message.value = '';
-        document.getElementById('form-success').style.display = 'block';
-        
-        // Hide success message after 5 seconds
-        setTimeout(() => {
-            document.getElementById('form-success').style.display = 'none';
-        }, 3000);
-        return;
-    }
-});
 
-// This is for the skill bars
-const skillProgress = document.querySelectorAll('.skill-progress');
-skillProgress.forEach(progress => {
-    const attri = progress.getAttribute("data-width");
-    progress.style.width = `${attri}%`;
-})
 
 
 
